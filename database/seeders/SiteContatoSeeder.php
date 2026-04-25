c756eaae32c84e1001cd1caf07c187045ca38acb
Felipe <felipe.macedo00@gmail.com>
2026-04-25 15:53:19 -0300

    Factories (semeando tabelas en massa com a dependência Faker)

--- database/seeders/SiteContatoSeeder.php
+++ database/seeders/SiteContatoSeeder.php
@@ -12,12 +12,6 @@ class SiteContatoSeeder extends Seeder
      */
     public function run(): void
     {
-        $contato = new SiteContato;
-        $contato->nome = 'Sistema SG';
-        $contato->telefone = '(11) 99999-8888';
-        $contato->email = 'contato@sg.com.br';
-        $contato->motivo_contato = 1;
-        $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão';
-        $contato->save();
+        SiteContato::factory()->count(100)->create();
     }
 }
