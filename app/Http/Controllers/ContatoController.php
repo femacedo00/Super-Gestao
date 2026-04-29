6ced994a1ef08fd671e750aea7cad335290844b3
Felipe <felipe.macedo00@gmail.com>
2026-04-29 10:42:17 -0300

    Validação de quantidades mínimas e máximas de caracteres

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -11,14 +11,15 @@ class ContatoController extends Controller
     {
         return view('site.contato', ['titulo' => 'Contato']);
     }
-    
-    public function salvar(Request $request) {
+
+    public function salvar(Request $request)
+    {
         $request->validate([
-            'nome' => 'required',
+            'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
             'email' => 'required',
             'motivo_contato' => 'required',
-            'mensagem' => 'required',
+            'mensagem' => 'required|max:2000',
         ]);
         SiteContato::create($request->all());
     }
