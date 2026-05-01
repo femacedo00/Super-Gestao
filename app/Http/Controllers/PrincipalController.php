29abea58ea5b22c3de67e38c0262aada167773fa
Felipe <felipe.macedo00@gmail.com>
2026-05-01 15:30:40 -0300

    Ajustando o formulário de contato na rota principal

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -6,6 +6,12 @@ class PrincipalController extends Controller
 {
     public function Principal()
     {
-        return view('site.principal');
+        $motivos_contatos = [
+            1 => 'Dúvida',
+            2 => 'Elogio',
+            3 => 'Reclamação',
+        ];
+
+        return view('site.principal', ['motivos_contatos' => $motivos_contatos]);
     }
 }
