1144f5eb470c26ce3a5e62117cd3d6aaf9c1efc6
Felipe <felipe.macedo00@gmail.com>
2026-05-09 08:30:40 -0300

    Criando o model LogAcesso e sua migration

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\LogAcesso;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,10 @@ class LogAcessoMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         // return $next($request);
-        return Response('Aqui é o middleware');
+        $ip = $request->server->get('REMOTE_ADDR');
+        $route = $request->getRequestUri();
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $route"]);
+
+        return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::middleware(LogAcessoMiddleware::class)
     ->get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::middleware(LogAcessoMiddleware::class)->get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
 Route::get('/login', function () {
